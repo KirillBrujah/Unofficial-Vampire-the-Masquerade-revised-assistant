2c69806f2c71ec2499ad8b15f072c89687d5d8c8
kirillbrujah <kirillbrujah@gmail.com>
2023-04-13 12:03:56 +0200

    Create fragment with viewmodel for characters

---
+++ app/src/main/java/com/cainites/unofficialvtmrevisedassistant/characters/CharactersFragment.kt
@@ -0,0 +1,32 @@
+package com.cainites.unofficialvtmrevisedassistant.characters
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.cainites.unofficialvtmrevisedassistant.R
+
+class CharactersFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = CharactersFragment()
+    }
+
+    private lateinit var viewModel: CharactersViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_characters, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(CharactersViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/cainites/unofficialvtmrevisedassistant/characters/CharactersViewModel.kt
@@ -0,0 +1,7 @@
+package com.cainites.unofficialvtmrevisedassistant.characters
+
+import androidx.lifecycle.ViewModel
+
+class CharactersViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
